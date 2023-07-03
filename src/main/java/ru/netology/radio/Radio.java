39db36053d1c434b0a746bc5fd1c2f5bc64e7006
Alex <aleksei.erof@gmail.com>
2023-07-03 11:46:24 +0500

    radio

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,98 @@
+package ru.netology.radio;
+
+public class Radio {
+
+    private int maxVolume = 100;
+    private int minVolume = 0;
+    private int maxChannel = 9;
+    private int minChannel = 0;
+    private int currentVolume = minVolume;
+    private int currentChannel = minChannel;
+
+    public Radio(int size) {
+        maxChannel = minChannel + size - 1;
+    }
+
+    public Radio() {
+        // this.minChannel = minChannel;
+        // this.maxChannel = maxChannel;
+        // this.currentChannel = minChannel;
+    }
+
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public int getMaxChannel() {
+        return maxChannel;
+    }
+
+    public int getMinChannel() {
+        return minChannel;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < minVolume) {
+            return;
+        }
+        if (newCurrentVolume > maxVolume) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public void setCurrentChannel(int newCurrentChannel) {
+        if (newCurrentChannel < minChannel) {
+            return;
+        }
+        if (newCurrentChannel > maxChannel) {
+            return;
+        }
+        currentChannel = newCurrentChannel;
+    }
+
+    public void setNextChannel() {
+        if (currentChannel < maxChannel) {
+            currentChannel = currentChannel + 1;
+        } else {
+            currentChannel = minChannel;
+        }
+    }
+
+    public void setPrevChannel() {
+        if (currentChannel > minChannel) {
+            currentChannel = currentChannel - 1;
+        } else {
+            currentChannel = maxChannel;
+        }
+    }
+
+    public void setPlusVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume = currentVolume + 1;
+        } else {
+            currentVolume = maxVolume;
+        }
+    }
+
+    public void setMinusVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        } else {
+            currentVolume = minVolume;
+        }
+    }
+}
